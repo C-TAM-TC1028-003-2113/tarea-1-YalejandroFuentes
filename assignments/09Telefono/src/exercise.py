90c8e08b91bb1c83189d608b9a1885dccbb126fd
6012 <A01412715@itesm.mx>
2021-08-31 11:57:27 -0500

    Termine los problemas.

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,9 +1,9 @@
 def main():
     # Este programa calcula el costo total mensual de un usuario del servicio telefonico.
 
-    Mensajes = int(input("Dame el numero de mensajes: "))
-    Megas = float(input("Dame el numero de megas: "))
-    Minutos = int(input("Dame el numero de minutos: "))
+    Mensajes = int(input("Dame el número de mensajes: "))
+    Megas = float(input("Dame el número de megas: "))
+    Minutos = int(input("Dame el número de minutos: "))
 
     CostoTotal= (Mensajes * 0.80) + (Megas * 0.80) + (Minutos * 0.80)
 
